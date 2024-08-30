17454bfa170ed19e9d1c507314c54aa37f231ad0
gonzaloconcep <lasconchasgonzalo@gmail.com>
2024-08-30 20:03:40 +0200

    Feat: add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind"
-
 import robotsTxt from "astro-robots-txt"
+
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   integrations: [tailwind(), robotsTxt()],
   site: 'https://porfolio.dev/'
 })
