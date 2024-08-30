38f0eaa9c323b2a73df6edc43bdbf9ae373b21c8
gonzaloconcep <lasconchasgonzalo@gmail.com>
2024-08-30 19:53:59 +0200

    Fix: delete project bocatasmanolo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-import robotsTxt from "astro-robots-txt"
-import vercel from '@astrojs/vercel/serverless';
 
+import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/',
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+  site: 'https://porfolio.dev/'
 })
